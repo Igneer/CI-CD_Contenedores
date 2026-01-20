6f20a039af180312b068d5d7c701e1c5f9db2428
MPrrado <matias14prado@gmail.com>
2026-01-20 16:55:31 -0300

    Agrego caso division por cero

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,7 +1,10 @@
 class Calculadora:
     def __init__(self):
         print ("CALCULADORA CREADA")
+              
     def division(self, a:int, b:int):
+        if b == 0:
+            return Exception
         return 0 #con error para luego aplicar CI
     
     def suma(self, a, b):
