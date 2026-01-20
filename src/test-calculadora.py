cb1e4730aa3ba50f4702dba00c7f7e3815146fba
MPrrado <matias14prado@gmail.com>
2026-01-20 17:14:38 -0300

    mejora en test para poder correr todos los test y tener un resumen correcto de todos los problemas

--- src/test-calculadora.py
+++ src/test-calculadora.py
@@ -4,23 +4,29 @@ from calculadora import *
 calculador = Calculadora()
 
 #Test para división
-assert calculador.division(4,0) == Exception # Pasa
-assert calculador.division(4,2) == 2 # No pasa
-assert calculador.division(15,3) == 5 # No pasa
-assert calculador.division(1,3) == 0 # Pasa
-assert calculador.division(3,3) == 1 # No pasa
+def test_div():
+    assert calculador.division(4,0) == Exception # Pasa
+    assert calculador.division(4,2) == 2 # No pasa
+    assert calculador.division(15,3) == 5 # No pasa
+    assert calculador.division(1,3) == 0 # Pasa
+    assert calculador.division(3,3) == 1 # No pasa
 
 #Test para resta 
-assert calculador.resta(5,1) == 4 # No pasa
-assert calculador.resta(3,0) == 3 # Pasa 
-assert calculador.resta(0,10) == -10# No para 
+def test_resta():
+    assert calculador.resta(5,1) == 4 # No pasa
+    assert calculador.resta(3,0) == 3 # Pasa 
+    assert calculador.resta(0,10) == -10# No para 
 
 #Test para multiplicación 
-
-assert calculador.multiplicacion(4,3) == 12# No pasa
-assert calculador.multiplicacion(3,1) == 3 # Pasa 
-assert calculador.multiplicacion(1,14) == 14 # No pasa
-assert calculador.multiplicacion(7,0) == 0 # No pasa
-assert calculador.multiplicacion(0,8) == 0 # No pasa
+def test_mult():
+    assert calculador.multiplicacion(4,3) == 12# No pasa
+    assert calculador.multiplicacion(3,1) == 3 # Pasa 
+    assert calculador.multiplicacion(1,14) == 14 # No pasa
+    assert calculador.multiplicacion(7,0) == 0 # No pasa
+    assert calculador.multiplicacion(0,8) == 0 # No pasa
 
 #Test para suma 
+
+# test_div()
+# test_resta()
+# test_mult()
