f3c14c29602eeff355cb5e1aa8223e4fdd189ac0
MPrrado <matias14prado@gmail.com>
2026-01-20 16:54:54 -0300

    test division completos

--- src/test-calculadora.py
+++ src/test-calculadora.py
@@ -2,4 +2,9 @@ from calculadora import *
 
 #agregar test 
 calculador = Calculadora()
-assert calculador.division(4,2) == 2
+
+assert calculador.division(4,0) == Exception # este pasa
+assert calculador.division(4,2) == 2 # este no pasa
+assert calculador.division(15,3) == 5 # este no pasa
+assert calculador.division(1,3) == 0 # este si pasa
+assert calculador.division(3,3) == 1 # este no pasa
